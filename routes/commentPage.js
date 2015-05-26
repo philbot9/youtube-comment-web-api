73ca38590fb675e9e5058f47b1c423a1979a8645
Philip Klostermann <philip.klostermann@gmail.com>
2015-05-25 21:16:08 -0400

    add support for  different videoIDs / cleanup

--- routes/commentPage.js
+++ routes/commentPage.js
@@ -1,37 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-var CommentPager = require('../lib/comment-pager');
-var getCommentPage;
+var getCommentPage = require('../lib/comment-pager');
 
-/* GET comment Page. */
+// POST comment Page - returns a page of comments including replies
 router.post('/', function(req, res, next) {
-  if(!req.body) {
-    return res.status(400);
-  }
-  if(!req.body.videoID) {
-    return res.status(400).json({
+  if(!req.body) return res.status(400);
+  
+  var videoID = req.body.videoID || null;
+  if(!videoID) {
+   return res.status(400).json({
       error: 'Missing field \'videoID\''
     });
   }
-
-  if(!getCommentPage) {
-    try {
-      getCommentPage = CommentPager({videoID: req.body.videoID});
-    } catch(err) {
-      return res.status(400).json({
-        error: 'Invalid Video ID'
-      });
-    }
-  }
-
-  getCommentPage(req.body.pageToken, function(error, page){
+  
+  var pageToken = req.body.pageToken || null;
+ 
+  getCommentPage(videoID, pageToken, function(error, page){
     if(error) {
+      console.error(error);
       return res.status(400).json({
         error: 'Fetching comment page failed.'
       });
     }
-      
+
     res.status(200).json({
       pageToken: req.body.pageToken,
       nextPageToken: page.nextPageToken,
